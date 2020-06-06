43e5bb5d0842172d1448831d4fd508e132166703
Segawa Shoko <shoko4prog@gmail.com>
2020-06-07 03:22:57 +0900

    ハンバーガーメニュー実装

--- resources/js/app.js
+++ resources/js/app.js
@@ -31,23 +31,25 @@ require('./bootstrap');
 //     el: '#app',
 // });
 
-// window.onload = function onload(){
-//     var hb_btn=document.getElementById("hb_menu_btn");
-//     var hb_menu=document.getElementById("hb_menu");
-//     var cover=document.getElementById("darker");
-//     var hb_btn_img=document.getElementById("hb_menu_btn_img");
-//     console
-//     hb_btn.addEventListener("click", function() {
-//         if(hb_btn.classList.contains("open")){
-//             hb_btn.className = "";
-//             hb_menu.className="hb_menu_close";
-//             cover.style.visibility="hidden";
-//             hb_btn_img.setAttribute('src','https://tanp_mission.jp/image/bars.png');
-//         }else{
-//             hb_btn.className = "open";
-//             hb_menu.className="hb_menu_open";
-//             cover.style.visibility="visible";
-//             hb_btn_img.setAttribute('src','https://tanp_mission.jp/image/x.png');
-//         }
-//     });
-// }
+window.onload = function onload(){
+    var hb_btn=document.getElementById("hb_menu_btn");
+    var hb_menu=document.getElementById("hb_menu");
+    var cover=document.getElementById("darker");
+    var hb_btn_img=document.getElementById("hb_menu_btn_img");
+    $('html').removeClass('scroll-prevent');
+    hb_btn.addEventListener("click", function() {
+        if(hb_btn.classList.contains("open")){
+            hb_btn.className = "";
+            hb_menu.className="hb_menu_close";
+            cover.style.visibility="hidden";
+            hb_btn_img.setAttribute('src','https://tanp_mission.jp/image/icons/menu.png');
+            $('html').removeClass('scroll-prevent');
+        }else{
+            hb_btn.className = "open";
+            hb_menu.className="hb_menu_open";
+            cover.style.visibility="visible";
+            hb_btn_img.setAttribute('src','https://tanp_mission.jp/image/icons/x.png');
+            $('html').addClass('scroll-prevent');
+        }
+    });
+}
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,9 +5,20 @@
             <button onfocus="this.blur();">
                 <img src="{{ asset( 'image/icons/cart.png',true)}}" alt="cart" class="header_icon">
             </button>
-            <button onfocus="this.blur();">
-                <img src="{{ asset( 'image/icons/menu.png',true)}}" alt="menu" class="header_icon">
+            <button onfocus="this.blur();" id="hb_menu_btn">
+                <img src="{{ asset( 'image/icons/menu.png',true)}}" alt="menu" class="header_icon" id="hb_menu_btn_img">
             </button>
         </div>
     </header>
+    <div id="hb_menu" class="hb_menu_close">
+        <p>こんにちは、〇〇さん</p>
+        <ul>
+            <a href="#"><li>注文履歴</li></a>
+            <a href="#"><li>記念日リマインダー</li></a>
+            <a href="#"><li>お気に入り</li></a>
+            <a href="#"><li>登録確認/修正/退会</li></a>
+            <a href="#"><li>ログアウト</li></a>
+        </ul>
+    </div>
+    <div id="darker"></div>
 @endsection
