e1c37ee8b5cfb5be8333cbcdbfbbd368cdb341d4
Segawa Shoko <shoko4prog@gmail.com>
2020-07-08 04:30:23 +0900

    バグ解消

--- app/Http/Controllers/Main/ProductFavoriteController.php
+++ app/Http/Controllers/Main/ProductFavoriteController.php
@@ -2,14 +2,12 @@
 
 namespace App\Http\Controllers\Main;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Model\Favorite;
+use Illuminate\Http\Request;
 
-class ProductfavoriteController extends Controller
+class ProductFavoriteController extends Controller
 {
-    public function __invoke(Request $request)
-    {
+    public function __invoke(Request $request){
         $favorite=Favorite::create(['user_id'=>$request->user_id,'product_id'=>$request->product_id]);
         return redirect()->back();
     }
