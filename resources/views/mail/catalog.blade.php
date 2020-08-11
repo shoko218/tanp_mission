b0d941cd4b10d3329a249c5efaf35ac386ee7ec4
Segawa Shoko <shoko4prog@gmail.com>
2020-08-11 22:30:17 +0900

    ロゴ変更

--- resources/views/mail/catalog.blade.php
+++ resources/views/mail/catalog.blade.php
@@ -147,18 +147,11 @@
 
 <body bgcolor="#ff9999" width="100%" style="Margin: 0;">
     <center style="width: 100%; background: #ffd1d1;">
-
-        <!-- Visually Hidden Preheader Text : BEGIN -->
-        <div style="display:none;font-size:1px;line-height:1px;max-height:0px;max-width:0px;opacity:0;overflow:hidden;mso-hide:all;font-family: sans-serif;">
-            (Optional) This text will appear in the inbox preview, but not the email body.
-        </div>
-        <!-- Visually Hidden Preheader Text : END -->
-
         <!-- Email Header : BEGIN -->
         <table cellspacing="0" cellpadding="0" border="0" align="center" width="600" style="margin: auto;" class="email-container">
 			<tr>
 				<td style="padding: 20px 0; text-align: center">
-					<img src="data:image/png;base64,{{base64_encode(file_get_contents(resource_path('image/logo.png')))}}" height="50" alt="alt_text" border="0">
+					<img src="data:image/png;base64,{{base64_encode(file_get_contents(resource_path('image/logo.png')))}}" height="80" alt="alt_text" border="0">
 				</td>
 			</tr>
         </table>
