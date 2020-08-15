4b6f87631725a2eb77378c95a972271006c7a493
Segawa Shoko <shoko4prog@gmail.com>
2020-08-15 23:42:58 +0900

    ユーザー登録時のバリデーション エラー表示設定

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,7 +10,6 @@
 
 @section('content')
     <section id="register">
-        @include('components.msgs')
         <h1>新規登録</h1>
         <p><span class="form_requires">*</span>は必須項目です</p>
         <form method="POST" action="{{ route('register') }}" class="input_form">
@@ -18,26 +17,44 @@
             <ul class="inputs">
                 <li class="input_parts">
                     <label for="last_name">姓<span class="form_requires">*</span></label>
-                    <input id="last_name" type="text" name="last_name" value="{{ old('last_name') }}" placeholder="山田" required autofocus>
+                    <input id="last_name" type="text" name="last_name" value="{{ old('last_name') }}" @if ($errors->has('last_name')) class="input_alert" @endif placeholder="山田" required autofocus>
+                    @foreach ($errors->get('last_name') as $item)
+                        <p class="form_alert">{{ $item }}</p>
+                    @endforeach
                 </li>
                 <li class="input_parts">
                     <label for="first_name">名<span class="form_requires">*</span></label>
-                    <input id="first_name" type="text" name="first_name" value="{{ old('first_name') }}" placeholder="太郎" required>
+                    <input id="first_name" type="text" name="first_name" value="{{ old('first_name') }}" @if ($errors->has('first_name')) class="input_alert" @endif placeholder="太郎" required>
+                    @foreach ($errors->get('first_name') as $item)
+                        <p class="form_alert">{{ $item }}</p>
+                    @endforeach
                 <li class="input_parts">
                     <label for="last_name_furigana">セイ<span class="form_requires">*</span></label>
-                    <input id="last_name_furigana" type="text" name="last_name_furigana" value="{{ old('last_name_furigana') }}" placeholder="ヤマダ" required autofocus>
+                    <input id="last_name_furigana" type="text" name="last_name_furigana" value="{{ old('last_name_furigana') }}" @if ($errors->has('last_name_furigana')) class="input_alert" @endif placeholder="ヤマダ" required autofocus>
+                    @foreach ($errors->get('last_name_furigana') as $item)
+                        <p class="form_alert">{{ $item }}</p>
+                    @endforeach
                 </li>
                 <li class="input_parts">
                     <label for="first_name_furigana">メイ<span class="form_requires">*</span></label>
-                    <input id="first_name_furigana" type="text" name="first_name_furigana" value="{{ old('first_name_furigana') }}" placeholder="タロウ" required>
+                    <input id="first_name_furigana" type="text" name="first_name_furigana" value="{{ old('first_name_furigana') }}" @if ($errors->has('first_name_furigana')) class="input_alert" @endif placeholder="タロウ" required>
+                    @foreach ($errors->get('first_name_furigana') as $item)
+                        <p class="form_alert">{{ $item }}</p>
+                    @endforeach
                 </li>
                 <li class="input_parts">
                     <label for="email">メールアドレス<span class="form_requires">*</span></label>
-                    <input id="email" type="email" name="email" value="{{ old('email') }}" placeholder="example@mail.com" required autocomplete="email">
+                    <input id="email" type="email" name="email" value="{{ old('email') }}" placeholder="example@mail.com" @if ($errors->has('email')) class="input_alert" @endif required autocomplete="email">
+                    @foreach ($errors->get('email') as $item)
+                        <p class="form_alert">{{ $item }}</p>
+                    @endforeach
                 </li>
                 <li class="input_parts">
                     <label for="password">パスワード<span class="form_requires">*</span></label>
-                    <input id="password" type="password" name="password" placeholder="password" required>
+                    <input id="password" type="password" name="password" placeholder="password" @if ($errors->has('password')) class="input_alert" @endif required>
+                    @foreach ($errors->get('password') as $item)
+                        <p class="form_alert">{{ $item }}</p>
+                    @endforeach
                 </li>
                 <li class="input_parts">
                     <label for="password-confirm">確認用パスワード<span class="form_requires">*</span></label>
@@ -45,7 +62,10 @@
                 </li>
                 <li class="input_parts">
                     <label for="birthday">誕生日<span class="form_requires">*</span></label>
-                    <input id="birthday" type="date" name="birthday" value="{{ old('birthday') }}" required>
+                    <input id="birthday" type="date" name="birthday" value="{{ old('birthday') }}" required @if ($errors->has('')) class="input_alert" @endif>
+                    @foreach ($errors->get('birthday') as $item)
+                        <p class="form_alert">{{ $item }}</p>
+                    @endforeach
                 </li>
                 <li class="radio_parts">
                     <p class="radiobtns_label">性別<span class="form_requires">*</span></p>
@@ -54,27 +74,42 @@
                         <label class="radio"><input type="radio" name="gender" value="1" class="radiobtn" @if(old('gender')==='1')checked="checked"@endif>女性</label>
                         <label class="radio"><input type="radio" name="gender" value="2" class="radiobtn" @if(old('gender')==='2')checked="checked"@endif>その他</label>
                     </div>
+                    @foreach ($errors->get('gender') as $item)
+                        <p class="form_alert">{{ $item }}</p>
+                    @endforeach
                 </li>
                 <li class="input_parts">
                     <label for="postal_code">郵便番号(ハイフン抜き)</label>
-                    <input id="postal_code" type="text" name="postal_code" value="{{ old('postal_code') }}" placeholder="xxxxxxx">
+                    <input id="postal_code" type="text" name="postal_code" value="{{ old('postal_code') }}" @if ($errors->has('postal_code')) class="input_alert" @endif placeholder="xxxxxxx">
+                    @foreach ($errors->get('postal_code') as $item)
+                        <p class="form_alert">{{ $item }}</p>
+                    @endforeach
                 </li>
                 <li class="input_parts">
                     <label for="prefecture_id">都道府県</label>
-                    <select name="prefecture_id" id="prefecture_id">
+                    <select name="prefecture_id" id="prefecture_id" @if ($errors->has('prefecture_id')) class="input_alert" @endif>
                         <option value="" selected>選択してください</option>
                         @foreach ($prefectures as $pref)
                             <option value="{{ $pref->id }}"@if(old('prefecture_id')==$pref->id) selected @endif>{{ $pref->name }}</option>
                         @endforeach
                     </select>
+                    @foreach ($errors->get('prefecture_id') as $item)
+                        <p class="form_alert">{{ $item }}</p>
+                    @endforeach
                 </li>
                 <li class="input_parts">
                     <label for="address">住所(市町村以下)</label>
-                    <textarea id="address" type="text" name="address" value="{{ old('address') }}" placeholder="〇〇市〇〇町x-xx〇〇ハイツxxx号室" rows="2"></textarea>
+                    <textarea id="address" type="text" name="address" value="{{ old('address') }}" placeholder="〇〇市〇〇町x-xx〇〇ハイツxxx号室" rows="2" @if ($errors->has('address')) class="input_alert" @endif>{{ old('address') }}</textarea>
+                    @foreach ($errors->get('address') as $item)
+                        <p class="form_alert">{{ $item }}</p>
+                    @endforeach
                 </li>
                 <li class="input_parts">
                     <label for="telephone">電話番号(ハイフン抜き)</label>
-                    <input id="telephone" type="text" name="telephone" value="{{ old('telephone') }}" placeholder="xxxxxxxxxx">
+                    <input id="telephone" type="text" name="telephone" value="{{ old('telephone') }}" placeholder="xxxxxxxxxx" @if ($errors->has('telephone')) class="input_alert" @endif>
+                    @foreach ($errors->get('telephone') as $item)
+                        <p class="form_alert">{{ $item }}</p>
+                    @endforeach
                 </li>
                 <li>
                     <label class="check">
