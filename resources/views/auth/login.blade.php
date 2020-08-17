afbd57c6785fb0ec7f6462f2c5f436b97e28c7ec
Segawa Shoko <shoko4prog@gmail.com>
2020-08-18 03:07:24 +0900

    ログイン時のダイアログ設定

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,14 +10,20 @@
 
 @section('content')
 <section id="login" class="normal_section">
-    @include('components.msgs')
+    @if (count($errors))
+    <div class="alert alert-danger alert-dismissible fade show msg_box">
+        <p>認証に失敗しました。
+        </p>
+        <button type="button" class="close" data-dismiss="alert">&times;</button>
+    </div>
+    @endif
     <h1>ログインしてください</h1>
     <form method="POST" action="{{ route('login') }}" class="input_form">
         @csrf
         <ul class="inputs">
             <li class="input_parts">
                 <label for="email">メールアドレス</label>
-                <input id="email" type="email" name="email" value="{{ old('email') }}" autocomplete="email" autofocus>
+                <input id="email" type="email" name="email" value="{{ old('email') }}" autocomplete="email" autofocus required>
                 @error('email')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -26,7 +32,7 @@
             </li>
             <li class="input_parts">
                 <label for="password">パスワード</label>
-                <input id="password" type="password" name="password" autocomplete="current-password">
+                <input id="password" type="password" name="password" autocomplete="current-password" required>
             </li>
             <li>
                 <label class="check" for="check"><input type="checkbox" value="check" class="checkbtn" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>ログイン情報を記憶する</label>
