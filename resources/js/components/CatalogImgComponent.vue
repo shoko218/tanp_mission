cb7c668adc288b700c44d4e257ce685bd4b52801
Segawa Shoko <shoko4prog@gmail.com>
2020-09-08 03:38:45 +0900

    先ほどのコミットの漏れ

--- resources/js/components/CatalogImgComponent.vue
+++ resources/js/components/CatalogImgComponent.vue
@@ -3,20 +3,20 @@
         <label for="img_num">イメージ画像</label>
         <select name="img_num" id="img_num" v-model="selectedImgNum" required>
             <option value="0" selected disabled>選択してください</option>
-            <option value="1">バースデーカード</option>
-            <option value="2">バースデーキャンドル</option>
-            <option value="3">バースデーガーランド</option>
             <option value="4">フラワー(ブルー)</option>
-            <option value="5">フラワー(ホワイト)</option>
-            <option value="6">サンキュー(キュート)</option>
-            <option value="7">サンキュー(エレガント)</option>
-            <option value="8">サンキュー(ポップ)</option>
+            <option value="5">フラワー(パステルピンク)</option>
+            <option value="13">フラワー(ホワイト)</option>
             <option value="9">バラ(ピンク)</option>
             <option value="10">バラ(ブルー)</option>
             <option value="11">ハート型の花(ピンク)</option>
             <option value="12">ハート型の花(ホワイト)</option>
-            <option value="13">フラワー(ホワイト)</option>
             <option value="14">ハート型の花(カラフル)</option>
+            <option value="1">バースデーカード</option>
+            <option value="2">バースデーキャンドル</option>
+            <option value="3">バースデーガーランド</option>
+            <option value="6">サンキュー(キュート)</option>
+            <option value="7">サンキュー(エレガント)</option>
+            <option value="8">サンキュー(ポップ)</option>
             <option value="15">サンキュー(シック)</option>
             <option value="16">サンキュー(フラワー)</option>
         </select>
