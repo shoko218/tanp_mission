4ce45e9cc545fb2522a0041520d1b6aa7ed583f8
Segawa Shoko <shoko4prog@gmail.com>
2020-09-27 05:05:35 +0900

    ・モーダル削除ボタンの場所調整 ・デモの注意書き

--- resources/views/main/cart.blade.php
+++ resources/views/main/cart.blade.php
@@ -10,6 +10,10 @@
 
 @section('content')
     <section id="cart" class="normal_section">
+        <div class="alert alert-danger alert-dismissible fade show msg_box">
+            ※このサービスはデモであり、購入処理をされても商品は届きません。<br>
+            また、クレジットカードの処理決済もデモですので引き落とされることはありません。
+        </div>
         <h1>お買い物カゴ</h1>
         @if (!count($products)==0||!count($cart_goods)==0)
         <cart-component :products='@json($products)' :cart-goods='@json($cart_goods)' :sum-price='@json($sum_price)' :product-count='@json($product_count)'></cart-component>
