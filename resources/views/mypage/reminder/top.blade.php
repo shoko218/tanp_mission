5b2c521cf916a4101e723fdad5005786fa6127a0
Segawa Shoko <shoko4prog@gmail.com>
2020-09-27 04:16:16 +0900

    微修正

--- resources/views/mypage/reminder/top.blade.php
+++ resources/views/mypage/reminder/top.blade.php
@@ -12,12 +12,16 @@
     <section id="event_reminder" class="normal_section">
         @include('components.msgs')
         <h1>イベントリマインダー</h1>
-        <div id="events">
-            @foreach ($events as $event)
-            @include('components.event_card',['date'=>$event->date,'name'=>$event->lover->last_name.$event->lover->first_name,'title'=>$event->title,'id'=>$event->id,'order'=>$loop->index,'ext'=>$event->lover->img_extension,'lover_id'=>$event->lover->id])
-            @endforeach
-        </div>
-        {{$events->appends(request()->input())->links()}}
+        @if(count($events)>0)
+            <div id="events">
+                @foreach ($events as $event)
+                @include('components.event_card',['date'=>$event->date,'name'=>$event->lover->last_name.$event->lover->first_name,'title'=>$event->title,'id'=>$event->id,'order'=>$loop->index,'ext'=>$event->lover->img_extension,'lover_id'=>$event->lover->id])
+                @endforeach
+            </div>
+            {{$events->appends(request()->input())->links()}}
+        @else
+            <h2>まだイベントはありません。</h2>
+        @endif
         <div class="btns">
             <button onclick="location.href='/mypage/reminder/register'">新しいイベントを登録→</button>
         </div>
