940a8a0a5dd3617316f4cb58389cf4b70d98e992
Segawa Shoko <shoko4prog@gmail.com>
2020-10-11 01:26:51 +0900

    EditProcessControllerのバグ修正

--- app/Http/Controllers/MyPage/Lovers/EditProcessController.php
+++ app/Http/Controllers/MyPage/Lovers/EditProcessController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\MyPage\Lovers;
 use App\Http\Controllers\Controller;
 use App\Model\Lover;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class EditProcessController extends Controller
 {
