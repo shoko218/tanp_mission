32dd881e976042fbacbdecbf229ca371e1c2c78a
Segawa Shoko <shoko4prog@gmail.com>
2020-11-19 04:41:48 +0900

    微修正

--- app/Http/Controllers/MyPage/Original_catalog/RemoveProcessController.php
+++ app/Http/Controllers/MyPage/Original_catalog/RemoveProcessController.php
@@ -12,14 +12,14 @@ class RemoveProcessController extends Controller
     public function __invoke(Request $request){//カタログから商品を取り除く
         try {
             $catalog=Catalog::find($request->catalog_id);
-            // if($catalog->did_send_mail){
-            //     return redirect('mypage/original_catalog')->with('err_msg','既に相手に送っているカタログは編集できません。');
-            // }
-            // DB::table('catalog_product')
-            // ->select('*')
-            // ->where('catalog_id','=',$request->catalog_id)
-            // ->where('product_id','=',$request->product_id)
-            // ->delete();
+            if($catalog->did_send_mail){
+                return redirect('mypage/original_catalog')->with('err_msg','既に相手に送っているカタログは編集できません。');
+            }
+            DB::table('catalog_product')
+            ->select('*')
+            ->where('catalog_id','=',$request->catalog_id)
+            ->where('product_id','=',$request->product_id)
+            ->delete();
         } catch (\Throwable $th) {
             return back()->with('err_msg','エラーが発生しました。');
         }
