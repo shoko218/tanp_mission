4f9fbce66865559f9545019399bcba99ad111e72
Segawa Shoko <shoko4prog@gmail.com>
2021-02-23 05:36:55 +0900

    検索メソッドの修正

--- app/Http/Controllers/Main/MakeResultUrlController.php
+++ app/Http/Controllers/Main/MakeResultUrlController.php
@@ -10,7 +10,7 @@ class MakeResultUrlController extends Controller
     public function __invoke(Request $request){ //検索URLを整形する
         $url = '/result?';
         if(Request('keyword') != null){
-            $url = $url.'keyword='.Request('keyword').'&';
+            $url = $url.'keyword='.Request('keyword');
         }else{
             if(Request('target_scene_id') != null){//シーン
                 $url = $url.'target_scene_id='.Request('target_scene_id').'&';
@@ -27,11 +27,11 @@ class MakeResultUrlController extends Controller
             if(Request('target_generation_id') != null){//世代
                 $url = $url.'target_generation_id='.Request('target_generation_id').'&';
             }
-        }
-        if(Request('sort_by') === null){
-            $url = $url.'sort_by=0';
-        }else{
-            $url = $url.'sort_by='.Request('sort_by');//ソート
+            if(Request('sort_by') === null){
+                $url = $url.'sort_by=0';
+            }else{
+                $url = $url.'sort_by='.Request('sort_by');//ソート
+            }
         }
         return redirect($url);
     }
